0aabe4e43e4700e78e2e267be658fc377c13f56b
llezic <151058853+llezic@users.noreply.github.com>
2024-05-16 11:41:55 +0000

    1234

--- script.js
+++ script.js
@@ -2,14 +2,16 @@
 let mySelect;
 let myPicker;
 let myRadio;
+var pg;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(255);
-  rect(0, 0, 130, 100);
+  pg = createGraphics(windowWidth, windowHeight);
 
   let button = createButton('clean');
   button.position(10, 80);
+  button.mousePressed(clean);
 
   myPicker = createColorPicker('deeppink');
   myPicker.position(10, 30);
@@ -19,6 +21,7 @@ function setup() {
   myRadio.size(150);
   myRadio.option('light');
   myRadio.option('dark');
+  myRadio.selected("light")
 
   // slider = createSlider(0, 255);
   // slider.position(10, 10);
@@ -41,12 +44,23 @@ function draw() {
   let color = myPicker.color();
   let radio = myRadio.value();
   
-
+  if (radio == 'light') {
+    background(255)
+  }
+  else if (radio == 'dark') {
+    background(0)
+  }
+  stroke("black")
+  fill("white")
+  rect(0, 0, 130, 100);
+  
   if (mouseIsPressed) {
     if ((mouseX > 150) || (mouseY > 150)) {
       if (select == "circle") {
-        fill(color);
-        circle(mouseX, mouseY, 100);
+        pg.fill(color);
+        pg.stroke(mouseY, mouseX, mouseX);
+        pg.strokeWeight(2);
+        pg.circle(mouseX, mouseY, 100);
       }
     }
   }
@@ -54,25 +68,31 @@ function draw() {
   if (mouseIsPressed) {
     if ((mouseX > 150) || (mouseY > 150)) {
       if (select == "square") {
-        fill(color);
-        square(mouseX, mouseY, 100);
+        pg.fill(color);
+        pg.stroke(mouseX, mouseY, mouseY);
+        pg.strokeWeight(2);
+        pg.square(mouseX, mouseY, 100);
       }
     }
   }
   if (mouseIsPressed) {
     if ((mouseX > 150) || (mouseY > 150)) {
       if (select == "triangle") {
-        fill(color);
-        triangle(mouseX, mouseY, mouseX-150, mouseY, mouseX-75, mouseY+75 );
+        pg.fill(color);
+        pg.stroke(mouseX, mouseY, mouseX);
+        pg.strokeWeight(2);
+        pg.triangle(mouseX, mouseY, mouseX-150, mouseY, mouseX-75, mouseY+75 );
       }
     }
   }
   // mySelect.style('margin', '20px, 20px');
   // myPicker.style('margin', '40px, 40px');
-  myRadio.style('display', 'flex', 'align-items', 'center', )
+  myRadio.style('display', 'flex', 'align-items', 'center', ) 
 
   
-  
- 
+  image(pg, 0, 0);
+}
 
+function clean() {
+  pg = createGraphics(windowWidth, windowHeight);
 }
